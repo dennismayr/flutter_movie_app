0bec3b4e93091c3653354d0937feeaa76c9a87bb
Dennis Mayr <dmayr.dev@gmail.com>
2019-07-03 00:03:33 -0400

    Added a repo to push data to BLOC

---
+++ lib/src/resources/repo.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'tmdb_api.dart';
+import '../models/movie_model.dart';
+
+class Repository {
+  final tmdbProvider = TMDBAPIprovider();
+
+  Future<ItemModel> getMovieList() => tmdbProvider.downloadList(); // we're pushing data to BLOC
+}
