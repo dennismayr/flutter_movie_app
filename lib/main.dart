99b6b38d9613023d85d889c2b142ece70a0fe138
Dennis Mayr <dmayr.dev@gmail.com>
2019-07-03 14:59:23 -0400

    Moved all app code to 'src/movie_app.dart'

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'src/movie_app.dart';      // most code will be here
+import 'src/movie_app.dart'; // most code will be here
 
-void main() => runApp(new MovieApp()); // main() starts program execution
-
-// Main app widget
-class MovieApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Películas Populares',
-      home: new MoviesList(),   // imported class MoviesList: where movies will be shown
-    );
-  }
+void main() {
+  runApp(MovieApp());
 }
