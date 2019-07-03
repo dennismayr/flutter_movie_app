78556abd33ae5b1a981639f3a7517c9ac3bb5e76
Dennis Mayr <dmayr.dev@gmail.com>
2019-07-02 23:44:10 -0400

    tMDB API access method - take 1

---
+++ lib/src/resources/tmdb_api.dart
@@ -0,0 +1,25 @@
+// tMDB API access method
+
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' show Client;
+
+class TMDBAPIprovider {
+  Client client = Client();
+  final _apiKey = 'e214cdebc17661b34492daa956c574ce'; // 'final' = 'let' in JS
+
+  // Now we're doing the async part of the API request to get our movie list info
+  Future<ItemModel> downloadList() async {
+    print("loaded");
+
+    final response = await client
+        .get("http://api.themoviedb.org/3/movie/popular?api_key=$_apiKey");
+    print(response.body.toString());
+
+    if (response.statusCode == 200) {
+      return ItemModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Could not load post');
+    }
+  }
+}
