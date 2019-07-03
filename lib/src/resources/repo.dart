044b876cf612cd4d42dac432640af4329566865d
Dennis Mayr <dmayr.dev@gmail.com>
2019-07-03 00:52:15 -0400

    Fixed class 'Repository' as 'Repo'

--- lib/src/resources/repo.dart
+++ lib/src/resources/repo.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'tmdb_api.dart';
 import '../models/movie_model.dart';
 
-class Repository {
+class Repo {
   final tmdbProvider = TMDBAPIprovider();
 
   Future<ItemModel> getMovieList() => tmdbProvider.downloadList(); // we're pushing data to BLOC
