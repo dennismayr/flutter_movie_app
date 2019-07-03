bc98624a986d6dbbff39fdb2f5a48459a92cbae6
Dennis Mayr <dmayr.dev@gmail.com>
2019-07-02 20:12:30 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/dmayr/dev/movie_app_1/MainActivity.kt
@@ -0,0 +1,13 @@
+package dmayr.dev.movie_app_1
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
