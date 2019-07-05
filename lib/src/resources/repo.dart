a1b8c790b88f81aebe9ab15af7aa3d3f6eab5299
Dennis Mayr <dmayr.dev@gmail.com>
2019-07-05 00:16:56 -0400

    getMovieList & getTrailers - re-done after accidental rollback

--- lib/src/resources/repo.dart
+++ lib/src/resources/repo.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 import 'tmdb_api.dart';
 import '../models/movie_model.dart';
+import '../models/trailer_model.dart';
 
 class Repo {
   final tmdbProvider = TMDBAPIprovider();
 
-  Future<ItemModel> getMovieList() => tmdbProvider.downloadList(); // we're pushing data to BLOC
+  Future<ItemModel> getMovieList() =>
+      tmdbProvider.downloadList(); // we're pushing data to BLOC
+  Future<TrailerModel> getTrailers(int movieId) =>
+      tmdbProvider.getTrailer(movieId);
 }
