edd56b1ce954dbdde962c32f4d6d1ad9e0ed12b0
Dennis Mayr <dmayr.dev@gmail.com>
2019-07-08 02:45:21 -0400

    Main view rewritten; added bottomNavigationBar: 'Inicio' & 'Favoritos'

--- lib/src/movie_app.dart
+++ lib/src/movie_app.dart
@@ -9,8 +9,50 @@ class MovieApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData.light(),
       title: 'Películas Populares',
-      home: Scaffold(
-        body: MovieList(),
+      home: SectionSwitcher(),  // This will switch between 'Inicio' and 'Favoritos'
+    );
+  }
+}
+
+// Section switcher definition
+class SectionSwitcher extends StatefulWidget {
+  SectionSwitcher({Key key}) : super(key: key);
+
+  @override
+  _SectionSwitcherState createState() => _SectionSwitcherState();
+}
+
+class _SectionSwitcherState extends State<SectionSwitcher> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+// Home view body
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MovieList(),
+      // TODO: View contents: must switch views 'Inicio' & 'Favoritos'
+
+// Bottom navigation bar:
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Inicio'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favoritos'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.indigo[400],
+        onTap: _onItemTapped,
       ),
     );
   }
